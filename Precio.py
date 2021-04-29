682647c7caaa7776dd5459f243c2c1c854404789
davidgfb <davidgracia99@gmail.com>
2021-04-29 18:20:45 +0200

    1

--- Precio.py
+++ Precio.py
@@ -1,9 +1,18 @@
 class Precio:
+    def __init__(self,coste):
+        self.setCoste(coste)
+
+    def setCoste(self,coste):
+        self.coste=coste
+
+    def getCoste(self):
+        return self.coste
+        
     def toString(self):
-        return "Precio:"
+        return "Precio:"+str(self.getCoste())
 
 '''
 #PROBADOR
-precio=Precio()
+precio=Precio(10)
 print(precio.toString())
 '''
